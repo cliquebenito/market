3810a20456a756fc91f8e5913750061bb6cc3097
cliquebenito <cliquebenito@mail.ru>
2024-04-04 20:54:31 +0300

    update

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"github.com/go-chi/chi/v5"
 	_ "github.com/jackc/pgx/v4/stdlib"
 	"log"
+	"net/http"
 	"project/config"
 	"project/internal/app"
+	"project/internal/routes"
 )
+
+func init() {
+
+	r := chi.NewRouter()
+	r.Get("/products", routes.AddProduct)
+	log.Println("server started")
+	http.ListenAndServe(":8080", r)
+
+}
 
 func main() {
 	// Configuration
@@ -16,6 +28,7 @@ func main() {
 
 	// Run
 	app.Run(cfg)
+
 }
 
 // https://github.com/ilyakaznacheev/cleanenv/blob/master/example/parse_multiple_files/main.go
